a276d5253a08d14a341e1c3106db7528e85cfb78
Jim Campbell <jimcampbell355@gmail.com>
2025-07-09 18:37:09 -0400

    Updated readme and main file

---
+++ Readme.md
@@ -0,0 +1,11 @@
+# Terraform Azure Resource Management
+
+
+## terraform-state
+
+* Directory to set up my terraform state storage in Azure through an Azure Storage Account
+* Apply once, and done
+
+## terraform-resources
+
+* Directory to control all my terraform resources, referencing the state from terraform-state
---
+++ terraform-state/main.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~>3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "management" {
+  name     = "JC-E1-RG"
+  location = "East US"
+}
+
+
+resource "azurerm_storage_account" "tf_state" {
+  name                            = "jce1tfstate"
+  resource_group_name             = azurerm_resource_group.management.name
+  location                        = azurerm_resource_group.management.location
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = false
+
+  tags = {
+    environment = "dev"
+  }
+}
+data "azurerm_client_config" "current" {}
+
+resource "azurerm_storage_container" "tfstate" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.tf_state.name
+  container_access_type = "private"
+}
